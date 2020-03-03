41e189fd260b2a02c5befaff8c6685def65df9d2
vijaysaravanan1812 <61357538+vijaysaravanan1812@users.noreply.github.com>
2020-03-03 11:36:13 +0530

    Add files via upload

---
+++ problem1.cpp
@@ -0,0 +1,48 @@
+using namespace std;
+
+#include<iostream>
+int main()
+{
+    int arr[]={2,3,5,8,4,1};
+    int size= sizeof(arr)/sizeof(arr[0]);
+    int r,k;
+    cout<<"Enter No of rotation"<<"\n";
+    cin>>k;
+    if (k>size)
+    {
+        r=k%6;
+    }
+    r=k;
+    
+    cout<<"The array is:"<<"\t";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"\n";
+
+    for (int i = 0; i < r; i++)
+    {
+ 
+        int temp;
+        temp=arr[size-1];
+        int j=(size-1);
+        while (j!=0)
+        {
+            arr[j]=arr[j-1];
+            j--;                
+        }
+        arr[j]=temp;
+
+ 
+    }
+    cout<<"\n";
+    cout<<"After rotation:"<<"\t";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    cout<<"\n";
+    return 0;
+    
+}
