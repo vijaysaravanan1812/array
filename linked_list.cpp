83415862c99ce1ef6eed4c468725228b47b0a2f6
vijaysaravanan1812 <vijay.saravanan@hotmail.com>
2020-03-20 18:31:04 +0530

    adding elements at last in linked list

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,45 +1,61 @@
 using namespace std;
 //linked list
 #include<iostream>
+int s;
 struct node
 {
     int data;
     node *next;
 };
-
+void insert(node *n)
+{
+    node *nextnode;
+    nextnode = new node();
+    cin>>nextnode -> data;
+    nextnode ->next = NULL;
+    while ( n ->next != NULL)
+    {
+        n = n ->next;
+    }
+    n ->next =  nextnode ;
+}
 void printlist(node *n)
 {
-    while (n != NULL)
+    while ( n != NULL)
     {
         cout<<n ->data<<" ";
         n = n ->next;
     }
+    cout<<"\n";
     
 }
 
 int main()
 {
+    int count = 0;
     node *first = NULL;
-    node *second = NULL;
-    node *third = NULL;
-
-
     first = new node();
-    third = new node();
-    second = new node();
-    
-    first ->data =  1;
-    first ->next = second;
-    
-   
-    second ->data = 21;
-    second ->next = third ;
+    cin>>first ->data;
+    first ->next = NULL;
 
-    
-    third ->data = 38;
-    third ->next = NULL;
-    
+    while (count != 10)
+    {
+        
+        cout<<"Do you wamt quit (Yes-1/No-0)";
+        cin>>s;
+        if (s== 1)
+            break;
+        else
+        {
+           insert(first);
+            printlist(first);
+            count ++;
+        }
+    }
+    cout<<"Final list :\n";
     printlist(first);
+
     return 0;
+ 
 
 }
